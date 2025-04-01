5e0bce2e58ecc96e28ca75c8c340c31979ab161a
Leonardo Cella <leeoceella@gmail.com>
2025-03-31 21:43:43 -0300

    alteraçoes

--- main.tf
+++ main.tf
@@ -4,7 +4,24 @@ module "vpc" {
 }
 module "loadbalancer" {
     source = "./modules/loadbalancer"
-    vpc_id = module.vpc.vpc_id
+    # Removed vpc_id as it is not expected by the frontend module
+    # subnet_pub01 removed as it is not expected by the frontend module
+    subnet_pub02 = module.vpc.subnet_pub02
+}
+
+module "frontend"{
+    source = "./modules/frontend"
+    # Removed vpc_id as it is not expected by the frontend module
     subnet_pub01 = module.vpc.subnet_pub01
     subnet_pub02 = module.vpc.subnet_pub02
+    subnet_priv01 = module.vpc.subnet_priv01
+    subnet_priv02 = module.vpc.subnet_priv02
+    project_name = var.project_name
+}
+
+module "backend" {
+    source = "./modules/backend"
+    # Removed vpc_id as it is not expected by the frontend module
+    subnet_priv01 = module.vpc.subnet_priv01
+    project_name = var.project_name
 }
--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -5,3 +5,38 @@ provider "aws" {
 resource "aws_security_group" "leocella_nginx_sg" {
   name_prefix = "leocella-backend-sg"
   description = "Security Group for Backend EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "backend" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.private_subnet_id.subnet_priv01.id
+  security_groups        = [aws_security_group.leocella_nginx_sg.id]
+  key_name               = var.key_pair_name
+
+  tags = {
+    Name = "leocella-backend-instance"
+  }
+}
